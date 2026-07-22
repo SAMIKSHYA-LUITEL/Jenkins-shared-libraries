805980397ab7868be4878b8dcda60d286b784345
SAMIKSHYA-LUITEL <117832992+SAMIKSHYA-LUITEL@users.noreply.github.com>
2026-07-22 12:09:53 +0545

    Fix indentation and formatting in docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,7 +3,7 @@ def call(String ProjectName, String ImageTag, String dockerHubUser){
     credentialsId:"dockerHubCred",
     usernameVariable:"dockerHubUser",
     passwordVariable:"dockerHubPass")]){
-  sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}
+  sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
 }
   sh "docker push ${dockerHubUser}/${ProjectName}:${ImageTag}"
 }
